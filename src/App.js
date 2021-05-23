a28ca6b99e2e5be3ea14035cd958d40fc0f904a3
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-05-24 01:04:19 +0530

    ADD: Login Functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return <div className="App"></div>;
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("user: ", user);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="App">{token ? <h1>I am logged in</h1> : <Login />}</div>
+  );
 }
 
 export default App;
