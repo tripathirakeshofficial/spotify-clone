964ade28703dff72d2b1f8db1c8cd01fd680dcdd
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-05-27 21:38:41 +0530

    ADD: Footer View

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,49 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I'm the footer</h1>
+      <div className="footer_left">
+        <img
+          className="footer_albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Usher_Cannes_2016_retusche.jpg"
+          alt=""
+        />
+        <div className="footer_songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  token: null,
-  //"BQB0unu7GlfwKrWexS18Sgd87_j_ktVx7896fi2dvU7zf1__dZIX_y3WAnvjd-rjgBaGvdPQ7Ahyyj7bXc8DVc1OPvFNQRlq4Jkd-GgUlqLbD-mjcUqmcoHEnQAQsHfEXu12zF4W96F3ar5QKOn5aA1jkZiJ2gXoCmGDbttuTRLX-HyE",
+  token:
+    "BQB0unu7GlfwKrWexS18Sgd87_j_ktVx7896fi2dvU7zf1__dZIX_y3WAnvjd-rjgBaGvdPQ7Ahyyj7bXc8DVc1OPvFNQRlq4Jkd-GgUlqLbD-mjcUqmcoHEnQAQsHfEXu12zF4W96F3ar5QKOn5aA1jkZiJ2gXoCmGDbttuTRLX-HyE",
 };
 
 const reducer = (state, action) => {
